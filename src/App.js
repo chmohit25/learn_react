b70ae5686b1f462b2ca273f07abd2fd8cd34b990
Mohit Chaudhary <chmohit25@gmail.com>
2020-05-24 19:24:01 +0530

    Route,Link,NavLink,Redirect,Prompt

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import {BrowserRouter as Router, Link,NavLink,Prompt,Redirect} from 'react-router-dom';
+import Route from 'react-router-dom/Route';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render(){
+      return (
+          <Router>
+
+            <div className="App">
+              <ul>
+                <li><Link to="/about/" >about</Link></li>
+                <li><NavLink to="/" exact activeStyle= {{color: 'red'}}>Home</NavLink></li>
+                <li><NavLink to="/user/Mohit" exact activeStyle= {{color: 'red'}}>Mohit</NavLink></li>
+                <li><NavLink to="/user/NotMohit" exact activeStyle= {{color: 'red'}}>NotMohit</NavLink></li>
+
+              </ul>
+              
+            <Route path="/" exact strict render= {
+              () => {
+                return (<p>Home</p>);
+              }
+            }/> 
+            <Route path="/about/" exact strict render= {
+              () => {
+                return (<p>About</p>);
+              }
+            }/> 
+              <Route path="/user/:username" exact strict render= {
+                ({match}) => {
+                  if (match.params.username == 'Mohit')
+                    return (<h1>Mohit's Page</h1>);
+                  return (<Redirect to = "/about/" /> );
+                }
+            }/> 
+                          
+            <Prompt when  message="true" />
+            <Prompt when  message = {
+              (location) => { console.log(location); 
+              return location.pathname == "/user/Mohit" ? "Sure? " : true }
+              }/>
+            </div>
+
+          </Router>
+
+      );
+  }
 }
 
 export default App;
